6a78d17ca99af52cbe3edf24400abceb5792942d
zcherradi2 <118278755+zcherradi2@users.noreply.github.com>
2024-10-14 03:26:21 +0100

    Update funcTest.py

--- funcTest.py
+++ funcTest.py
@@ -1,6 +1,18 @@
-expression = "x*2+1"
-def func(x):
-    return eval(expression)
-
-
-exec("print(func(1))")
+class ThreadFetchHandler:
+    def __init__(self,initVal,fetcher) -> None:
+        self.val = initVal
+        self.state = 'ready'
+        self.fetcher = fetcher
+    def updateVal(self):
+        if self.state == "pending":
+            return self.get()
+        self.state = 'pending'
+        self.val = self.fetcher()
+        self.state = 'ready'
+        print('got new token')
+        return self.val
+    def get(self):
+        if self.state == 'ready':
+            return self.val
+        time.sleep(0.25)
+        return self.get()
