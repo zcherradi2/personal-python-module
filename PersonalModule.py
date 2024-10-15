e13b79f97b9b2fc1572051ab31b3a462a8fe9f70
zcherradi2 <118278755+zcherradi2@users.noreply.github.com>
2024-10-16 00:09:53 +0100

    Update PersonalModule.py

--- PersonalModule.py
+++ PersonalModule.py
@@ -16,3 +16,30 @@ class ThreadFetchHandler:
             return self.val
         time.sleep(0.25)
         return self.get()
+class Writer:
+    writers = []
+    def __init__(self,file,rate=10) -> None:
+        self.file = file
+        self.rate = rate
+        self.pendingLines = []
+        self.writers.append(self)
+    def appendLine(self,line):
+        self.pendingLines.append(line)
+        if len(self.pendingLines)>=self.rate:
+            self.pushLines()
+    def pushLines(self):
+        res = ''
+        lines = self.pendingLines.copy()
+        self.pendingLines = []
+        for line in lines:
+            res += line+'\n'
+        with FileLock(f"./{self.file}.txt.lock"):
+            with open(self.file,'a') as file:
+                file.write(res)
+    def exit(self):
+        self.pushLines()
+    @classmethod
+    def killSession(cls):
+        for writer in cls.writers:
+            writer.exit()
+            print('+ writer killed')
